edd598cea426e261a2684c6fc7846d6819df695c
Matafight <guo_sc@foxmail.com>
2016-09-05 18:51:37 +0800

    examples

--- scrapy/tutorial/tutorial/spiders/news_nuaa_spider.py
+++ scrapy/tutorial/tutorial/spiders/news_nuaa_spider.py
@@ -1,11 +1,12 @@
 import scrapy
-
+from tutorial.items import TutorialItem
 class news_nuaa_spider(scrapy.Spider):
     name = 'news_nuaa_spider'
     start_urls = ['http://cs.nuaa.edu.cn/?p=16230']
     allowed_domains = ['cs.nuaa.edu.cn']
     def parse(self,response):
-        ans = response.xpath('//div[@class = "content"]').extract()
-        print(ans)
-
+        item = TutorialItem()
+        item['news_content'] = response.xpath('//h2[@class = "C"]').extract()
+        #print(item['news_content'][0])
+        yield item
         #pass
