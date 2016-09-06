ea55661c33a592300772bc06ef16b95e28e8c0f9
Matafight <guo_sc@foxmail.com>
2016-09-06 15:00:37 +0800

    能粗略地爬nuaa计算机学院网站的新闻了，还需要进一步优化

--- scrapy/tutorial/tutorial/spiders/news_nuaa_spider.py
+++ scrapy/tutorial/tutorial/spiders/news_nuaa_spider.py
@@ -1,18 +1,36 @@
 import scrapy
+import re
 from tutorial.items import news_nuaa_items
 class news_nuaa_spider(scrapy.Spider):
     name = 'news_nuaa_spider'
-    start_urls = ['http://cs.nuaa.edu.cn/?p=16230']
+    start_urls = ['http://cs.nuaa.edu.cn/?cat=12&paged=1']
+
     allowed_domains = ['cs.nuaa.edu.cn']
     def parse(self,response):
+
+        for page in range(1,10,1):
+            urls = 'http://cs.nuaa.edu.cn/?cat=12&paged='+str(page)
+            yield scrapy.Request(urls,callback = self.parse_urls)
+
+
+    def parse_urls(self,response):
+        exp = re.compile(r'<td\s+height="20"><a\s+href="(.*?)">')
+
+        resphtml = re.findall(exp,response.body)
+
+        for links in resphtml:
+            print(links)
+            yield scrapy.Request(links,callback = self.parse_items)
+    def parse_items(self,response):
         item = news_nuaa_items()
+
         #sles= response.xpath('//div[@class = "content"]')
         news_title = response.xpath('//h2[@class = "C"]/text()')
 
         for title in news_title:
             tartitle  = title.extract()
             item['title'] = tartitle.encode('utf-8')
-            print(item['title'])
+            #print(item['title'])
         news_time = response.xpath('//div[@class= "info"]/text()')
 
         for time in news_time:
